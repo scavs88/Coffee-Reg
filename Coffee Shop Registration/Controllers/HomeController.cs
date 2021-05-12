35019cc54956bc8d935a3ed363fef25049957c02
sean <spcavanaugh88@gmail.com>
2021-05-12 09:19:13 -0400

    coffee reg

---
+++ Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Registration reg)
+        {
+            ViewData["FirstName"] = reg.FirstName;
+            ViewData["LastName"] = reg.LastName;
+            ViewData["Password"] = reg.Password.Length;
+            ViewData["Email"] = reg.Email;
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
